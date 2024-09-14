f412e696ccfa7fcf1521b3cebe25465117e06f0a
david <david.saadoun@hotmail.com>
2024-09-14 11:50:42 +0200

    Updates

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager): Response
+    {
+        // Si l'utilisateur est déjà connecté, redirigez-le vers la page d'accueil
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encoder le mot de passe
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            // Assurez-vous que l'utilisateur a au moins le rôle "ROLE_USER"
+            $user->setRoles(['ROLE_USER']);
+
+            // Enregistrez l'utilisateur
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirigez vers la page de connexion
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+}
