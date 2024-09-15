c212bbde3cb3f8d1409dbce144cb63199f5748fa
david <david.saadoun@hotmail.com>
2024-09-15 21:33:55 +0200

    Updates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('home.html.twig');
+        // Récupérer les 3 produits mis en avant (highlighted)
+        $highlightedProducts = $productRepository->findBy(
+            ['highlighted' => true], // Condition : seulement les produits "mis en avant"
+            null,                    // Aucun tri particulier
+            3                        // Limite : 3 produits
+        );
+
+        return $this->render('home.html.twig', [
+            'highlightedProducts' => $highlightedProducts,
+        ]);
     }
 }
 
