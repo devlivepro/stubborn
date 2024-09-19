7c65b49572d9d974ff004224d6427f9230bc2fb8
david <david.saadoun@hotmail.com>
2024-09-19 14:47:07 +0200

    Add new file

---
+++ src/Form/ProductType.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('price', NumberType::class, [
+                'label' => false,
+                'scale' => 2,
+                'attr' => ['class' => 'form-control'],
+            ])
+            // Ajout des champs pour chaque taille (XS à XL)
+            ->add('stock_0', NumberType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control'],
+                'mapped' => false, // Non mappé directement à l'entité, on va le gérer manuellement
+            ])
+            ->add('stock_1', NumberType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control'],
+                'mapped' => false,
+            ])
+            ->add('stock_2', NumberType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control'],
+                'mapped' => false,
+            ])
+            ->add('stock_3', NumberType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control'],
+                'mapped' => false,
+            ])
+            ->add('stock_4', NumberType::class, [
+                'label' => false,
+                'attr' => ['class' => 'form-control'],
+                'mapped' => false,
+            ])
+            ->add('highlighted', CheckboxType::class, [
+                'label' => 'M\'être en avant',
+                'required' => false,
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'Image du produit',
+                'required' => false,
+                'mapped' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
