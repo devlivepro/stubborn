1433534b0db4bfe886a2b49b2c2840a5ff8d4d7b
david <david.saadoun@hotmail.com>
2024-09-26 12:35:25 +0200

    Updates

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -18,17 +18,41 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        // Création d'un utilisateur admin de test
-        $user = new User();
-        $user->setEmail('johndoe@example.com');
-        $user->setUsername('johndoe'); // Vérifiez que ce champ existe dans votre entité User
-        $user->setPassword($this->passwordHasher->hashPassword($user, 'password'));
-        $user->setRoles(['ROLE_ADMIN']);
-        $user->setDeliveryAddress('8 rue Paul Roberto, 75001 Paris');
-        $user->setIsAdmin(true);
-        $manager->persist($user);
+        // Création utilisateur 1
+        $user1 = new User();
+        $user1->setEmail('johndoe@hotmail.com');
+        $user1->setPassword($this->passwordHasher->hashPassword($user1, 'test1234'));
+        $user1->setRoles(['ROLE_USER']);
+        $user1->setUsername('johndoe');
+        $user1->setDeliveryAddress('5 rue de la voie 75001 Paris');
+        $user1->setIsVerified(true);
+        $user1->setIsAdmin(false);
+        $manager->persist($user1);
+        $manager->flush();
 
+        // Création utilisateur 2
+        $user2 = new User();
+        $user2->setEmail('johndoeadmin@hotmail.com');
+        $user2->setPassword($this->passwordHasher->hashPassword($user2, 'test1234'));
+        $user2->setRoles(['ROLE_USER']);
+        $user2->setUsername('johndoeadmin');
+        $user2->setDeliveryAddress('6 rue de la voie 75001 Paris');
+        $user2->setIsVerified(true);
+        $user2->setIsAdmin(true);
+        $manager->persist($user2);
+        $manager->flush();
+
+        // Création utilisateur 3
+        $user3 = new User();
+        $user3->setEmail('johndoe0@hotmail.com');
+        $user3->setPassword($this->passwordHasher->hashPassword($user3, 'test1234'));
+        $user3->setRoles(['ROLE_USER']);
+        $user3->setUsername('johndoe0');
+        $user3->setDeliveryAddress('7 rue de la voie 75001 Paris');
+        $user3->setIsVerified(false);
+        $user3->setIsAdmin(false);
+        $manager->persist($user3);
         $manager->flush();
     }
-    
+
 }
